f8a580457c5b5c39a304dcc482be9a68176ca595
Mariia <46101310+marijabolschakova@users.noreply.github.com>
2024-01-07 10:42:54 +0200

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '041c1f3c-68b5-4c82-ad5c-8f35d001b02f';
+export const MODAL_CLOSE_SEC = 2.5;
